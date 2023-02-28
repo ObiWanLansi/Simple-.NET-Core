88d2be159cbf83da434a054f4409d70f4fb09ffd
Jörg Lanser <38795604+ObiWanLansi@users.noreply.github.com>
2023-02-28 18:28:34 +0000

    Threads

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,14 +2,38 @@
 class Program
 {
 
-    private static void MakeTrouble()
+    public static void Counter(object delay)
     {
-        Thread.Sleep(2000);
+        ulong counter = 0;
+
+        int iDelay=(int) delay;
+
+        try
+        {
+            while (true)
+            {
+                Console.Out.WriteLineAsync($"{Thread.CurrentThread.ManagedThreadId}: {counter++}");
+                 Thread.Sleep(iDelay);
+            }
+        }
+        catch (Exception)
+        {
+
+        }
     }
 
 
     static void Main(string[] args)
     {
-        Console.Out.WriteLine("Hello, World!");
+        Thread t1 = new Thread(Counter);
+        Thread t2 = new Thread(Counter);
+
+        t1.Start(321);
+        t2.Start(123);
+
+        Thread.Sleep(5000);
+
+        t1.Interrupt();
+        t2.Interrupt();
     }
 }
