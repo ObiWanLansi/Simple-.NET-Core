327500e2363907c8fc2b3c9f041cfc087326699b
Jörg Lanser <38795604+ObiWanLansi@users.noreply.github.com>
2023-03-02 06:29:07 +0000

    Using Task

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -26,15 +26,28 @@ class Program
 
     static void Main(string[] args)
     {
-        Thread t1 = new Thread(Counter);
-        Thread t2 = new Thread(Counter);
+        #region Old School
+        Thread t1 = new Thread(Counter) { IsBackground = true };
+        Thread t2 = new Thread(Counter) { IsBackground = true }; ;
 
         t1.Start(321);
         t2.Start(123);
 
         Thread.Sleep(5000);
 
-        t1.Interrupt();
-        t2.Interrupt();
+        // t1.Interrupt();
+        // t2.Interrupt();
+        #endregion
+
+        //---------------------------------------------------------------------
+
+        #region New Style
+
+        // Task t1 = Task.Run(() => Counter(321));
+        // Task t2 = Task.Run(() => Counter(123));
+
+        // Thread.Sleep(5000);
+
+        #endregion
     }
 }
